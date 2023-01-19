640ee25fade942a3aace746a38d9bc929e430b99
dan <37590801+dan-auth0@users.noreply.github.com>
2023-01-18 19:45:38 -0500

    feat: remove json-server API for Vercel deploy

--- src/services/message.service.js
+++ src/services/message.service.js
@@ -1,54 +1,23 @@
-import { callExternalApi } from "./external-api.service";
-
-const apiServerUrl = process.env.REACT_APP_API_SERVER_URL;
-
 export const getPublicResource = async () => {
-  const config = {
-    url: `${apiServerUrl}/api/messages/public`,
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-    },
-  };
-
-  const { data, error } = await callExternalApi({ config });
-
   return {
-    data: data || null,
-    error,
+    data: {
+      text: "This is a public message.",
+    },
   };
 };
 
 export const getProtectedResource = async () => {
-  const config = {
-    url: `${apiServerUrl}/api/messages/protected`,
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-    },
-  };
-
-  const { data, error } = await callExternalApi({ config });
-
   return {
-    data: data || null,
-    error,
+    data: {
+      text: "This is a protected message.",
+    },
   };
 };
 
 export const getAdminResource = async () => {
-  const config = {
-    url: `${apiServerUrl}/api/messages/admin`,
-    method: "GET",
-    headers: {
-      "content-type": "application/json",
-    },
-  };
-
-  const { data, error } = await callExternalApi({ config });
-
   return {
-    data: data || null,
-    error,
+    data: {
+      text: "This is an admin message.",
+    },
   };
 };

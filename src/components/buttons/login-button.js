c0384e77bb520e67938ebc64f99cb2e6ec148baa
galbeiroc <alcresgu_1989@hotmail.com>
2023-07-17 23:43:28 -0500

    add login component

---
+++ src/components/buttons/login-button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin =  async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/profile'
+      }
+    })
+  }
+
+  return(
+    <button className="login_button" onClick={handleLogin}>
+      Log In
+    </button>
+  )
+}

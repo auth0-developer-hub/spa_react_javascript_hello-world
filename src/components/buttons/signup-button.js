d60d8cd7dc1c28d8f1d325a9b9e6d56eb285d421
galbeiroc <alcresgu_1989@hotmail.com>
2023-07-17 23:43:54 -0500

    add signup component

---
+++ src/components/buttons/signup-button.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const SignupButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleSignup = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/profile'
+      },
+      authorizationParams: {
+        screen_hint: 'signup'
+      }
+    })
+  }
+
+  return (
+    <button className="button__sign" onClick={handleSignup}>
+      Sign Up
+    </button>
+  )
+}
